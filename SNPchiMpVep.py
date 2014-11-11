3a8b6b628294d9f4f58e190102ea8dd9dc722e83
Paolo Cozzi <paolo.cozzi@tecnoparco.org>
2014-11-11 13:10:40 +0100

    Some little bugfix on make test

--- SNPchiMpVep.py
+++ SNPchiMpVep.py
@@ -37,7 +37,7 @@ print("Content-type: text/html\n")
 #cgi.test()
 
 #Logging istance
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG)
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger("SNPchiMpVep")
 
 logger.info("CGI script started")
